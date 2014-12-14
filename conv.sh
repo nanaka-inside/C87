f5584b2100f44688d7e4ce2758abed63dfe873e3
tboffice <tbofficed@gmail.com>
2014-12-14 17:12:45 +0900

    fix figure

---
+++ conv.sh
@@ -0,0 +1,6 @@
+md2inao.pl --format=in_design ./richmikan/index.md | nkf -s > richmikan.txt
+md2inao.pl --format=in_design ./neishan/index.md | nkf -s > neishan.txt
+md2inao.pl --format=in_design ./saiten/index.md | nkf -s > saiten.txt
+md2inao.pl --format=in_design ./bob/index.md | nkf -s > bob.txt
+md2inao.pl --format=in_design ./watanabe/index.md | nkf -s > watanabe.txt
+md2inao.pl --format=in_design ./tboffice/index.md | nkf -s > tboffice.txt
